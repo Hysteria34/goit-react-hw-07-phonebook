e17cec8849b41531f6331ffb8bce72f733f2e994
Anastasia Donchenko <88480169+Hysteria34@users.noreply.github.com>
2022-07-28 16:56:30 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useFetchContactsQuery } from './services/contactsAPI';
-
 import Container from './components/Container'
 import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList/';
@@ -9,15 +7,14 @@ import Loader from './components/Loader';
 import s from './App.module.css'
 
 export default function App() {
-  const { data: contacts, isFetching} = useFetchContactsQuery();
 
   return (
     <Container><h1 className={s.title}>Phonebook</h1>
-    <ContactForm contacts={contacts} />
+    <ContactForm/>
 
     <h2 className={s.title}>Contacts</h2>
     <Filter />
-    {isFetching && <Loader />}
-    <ContactList contacts={contacts} /></Container>
+    <Loader />
+    <ContactList/></Container>
   );
 }
